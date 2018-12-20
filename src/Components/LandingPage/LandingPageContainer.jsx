225e7a7a0f2fb06ad01d205b0e72702f6422b04f
biratsapkota <biratsapkota9@gmail.com>
2018-12-20 20:23:51 +0545

    commit2

--- src/Components/LandingPage/LandingPageContainer.jsx
+++ src/Components/LandingPage/LandingPageContainer.jsx
@@ -2,15 +2,37 @@ import React from 'react';
 import PopularCompanies from './PopularCompanies'; 
 
 class LandingPageContainer extends React.Component{
-  componentWillMount(){
-    
-  }
 	render(){
 		return (
 			<div>
-  			 Hero Part <br/>
-         <PopularCompanies />
+				<div class="searchwrapper">
+					<div class="forsearch">
+						<div class="searchinfo">
+							Get access to millions of company reviews
+						</div>
 
+						<div class="searchhead">
+							Find great places to work
+						</div>
+
+						<div class = "searchbars">
+							<div class = "searchbar">
+								<div>Company name or job title</div>
+								<input type="Text"/>
+							</div>
+
+							<div class = "searchbar">
+								<div>City,state,or zip(optional)</div>
+								<input type="Text"/>
+							</div>
+							<div class = "searchbuts">
+								<div class="searchbut">Find Companies</div>
+								<div class="searchbutinfo">Do you want to search for salaries?</div>
+							</div>
+						</div>
+					</div>		
+				</div> 			 
+	    		<PopularCompanies />
 			</div>
 		)
 	}
